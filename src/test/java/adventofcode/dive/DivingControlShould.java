1ade169b296fa0fcbd08501fff126f56ee5e1a93
oscargiol <giol.oscar@gmail.com>
2021-12-09 21:48:12 +0100

    oops, forgot to push this

--- src/test/java/adventofcode/dive/DivingControlShould.java
+++ src/test/java/adventofcode/dive/DivingControlShould.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DivingControlShould {
     private DivingControl divingControl = new DivingControl();
@@ -19,5 +19,25 @@ class DivingControlShould {
                 new Movement(Direction.DOWN, 8),
                 new Movement(Direction.FORWARD, 2)
         );
+
+        Position finalPosition = divingControl.perform(movements);
+
+        assertThat(finalPosition).isEqualTo(new Position(15, 10, 0));
+    }
+
+    @Test
+    void move_submarine_given_a_planned_course_with_aim() {
+        List<Movement> movements = List.of(
+                new Movement(Direction.FORWARD, 5),
+                new Movement(Direction.DOWN, 5),
+                new Movement(Direction.FORWARD, 8),
+                new Movement(Direction.UP, 3),
+                new Movement(Direction.DOWN, 8),
+                new Movement(Direction.FORWARD, 2)
+        );
+
+        Position finalPosition = divingControl.performWithAim(movements);
+
+        assertThat(finalPosition).isEqualTo(new Position(15, 60, 10));
     }
 }
